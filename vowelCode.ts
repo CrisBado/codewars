5d1fa5dbe1132c8087b4316cbafd0fd47edda6b6
Christian Baidoo <christian.baidoo@15gifts.com>
2025-06-12 17:11:31 +0100

    solve vowel code kata

---
+++ vowelCode.ts
@@ -0,0 +1,22 @@
+/*
+Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the 
+following pattern:
+a -> 1
+e -> 2
+i -> 3
+o -> 4
+u -> 5
+For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
+*/
+
+export function encode(string: string): string {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const as = string.split("");
+  const sd = as.map((a, i) => {
+    if (vowels.includes(a)) {
+      return vowels.indexOf(a) + 1;
+    }
+    return a;
+  });
+  return sd.join("");
+}
