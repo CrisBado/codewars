11791cccaa66b1e56c7b5bf3173679fc8ce5de48
Christian Baidoo <christian.baidoo@15gifts.com>
2025-07-03 22:43:17 +0100

    solve backspaces in string kata

---
+++ backSpace.ts
@@ -0,0 +1,23 @@
+/*
+Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
+
+Your task is to process a string with "#" symbols.
+
+Examples
+"abc#d##c"      ==>  "ac"
+"abc##d######"  ==>  ""
+"#######"       ==>  ""
+""              ==>  ""
+
+*/
+function cleanString(s: string): string {
+  const result: string[] = [];
+  for (const char of s) {
+    if (char === "#") {
+      result.pop();
+    } else {
+      result.push(char);
+    }
+  }
+  return result.join("");
+}
