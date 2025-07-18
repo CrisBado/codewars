c3e60acf5b9fd773158929e9333e26a994910cd2
Christian Baidoo <christian.baidoo@15gifts.com>
2025-07-18 23:52:09 +0100

    first commit sort the inner content in descending order kata

---
+++ sortTheInnerContentInDescendingOrder.ts
@@ -0,0 +1,17 @@
+/*
+You have to sort the inner content of every word of a string in descending order.
+
+The inner content is the content of a word without first and the last char.
+
+Some examples:
+
+"sort the inner content in descending order"  -->  "srot the inner ctonnet in dsnnieedcg oredr"
+"wait for me"        -->  "wiat for me"
+"this kata is easy"  -->  "tihs ktaa is esay"
+Words are made up of lowercase letters.
+*/
+
+function sortTheInnerContent(words: string): string {
+  console.log(words);
+  return words;
+}
