cd268fb01ce3b67f8acd070df12b468630bb5e02
Christian Baidoo <christian.baidoo@15gifts.com>
2025-10-15 12:18:20 +0100

    solve consecutiveString kata

--- consecutiveString.ts
+++ consecutiveString.ts
@@ -1,19 +1,22 @@
 export function longestConsec(strarr: string[], k: number): string {
   const result: string[] = [];
-  let currentString = "";
-  //   console.log(strarr)
 
-  for (let i = 0; i < strarr.length; i++) {
-    // Append the current element (converted to string)
-    currentString += strarr[i];
-    console.log(currentString);
+  for (let i = 0; i < strarr.length - k + 1; i++) {
+    let currentConcatenatedString = "";
 
-    // Check if the current chunk is complete (i.e., we've processed N elements)
-    if ((i + 1) % k === 0) {
-      result.push(currentString);
-      currentString = ""; // Reset for the next chunk
+    for (let j = 0; j < k; j++) {
+      currentConcatenatedString += strarr[i + j];
     }
+    result.push(currentConcatenatedString);
   }
 
-  return "sd";
+  const longestElement = result.reduce((longest, current) => {
+    if (current.length > longest.length) {
+      return current;
+    } else {
+      return longest;
+    }
+  }, "");
+
+  return longestElement;
 }
