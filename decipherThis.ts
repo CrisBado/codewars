546cbcd2601c7399e972f3bec1ebf4682885a972
Christian Baidoo <christian.baidoo@15gifts.com>
2025-10-30 11:45:58 +0000

    decipherThis kata first attempt

---
+++ decipherThis.ts
@@ -0,0 +1,31 @@
+/* 
+You are given a secret message you need to decipher. Here are the things you need to know to decipher it:
+
+For each word:
+
+the second and the last letter is switched (e.g. Hello becomes Holle)
+the first letter is replaced by its character code (e.g. H becomes 72)
+there are no special characters used, only letters and spaces
+words are separated by a single space
+there are no leading or trailing spaces
+Examples
+
+'72olle 103doo 100ya' --> 'Hello good day'
+'82yade 115te 103o'   --> 'Ready set go'
+*/
+
+export function decipherThis(str: string): string {
+  //     console.log(String.fromCharCode(72))
+
+  const array = str.split(" ");
+
+  return array
+    .map((e: string) => {
+      const secondLetter = "";
+      const firstLetter = String.fromCharCode(Number(e.charAt(0)));
+      const lastLetter = e.charAt(e.length - 1);
+      //       console.log(lastLetter )
+      //       return `${firstLetter}${lastLetter}${numberOfChar}`;
+    })
+    .join("");
+}
