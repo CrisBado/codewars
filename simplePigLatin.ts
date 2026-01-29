bf0a9c2be6c8213384a1c858ebbaa9256f10ddaa
Christian Baidoo <christian.baidoo@15gifts.com>
2026-01-29 21:09:58 +0000

    simple pig latin kata solved

---
+++ simplePigLatin.ts
@@ -0,0 +1,22 @@
+/* 
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+
+*/
+
+export const pigIt = (str: string): string => {
+  return str
+    .split(" ")
+    .map((word) => {
+      if (/^[A-Za-z]+$/.test(word)) {
+        const first = word.slice(0, 1);
+        const rest = word.slice(1);
+        return `${rest}${first}ay`;
+      }
+      return word;
+    })
+    .join(" ");
+};
