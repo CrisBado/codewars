8e5b939335951bb4f4cb1ce8bc1be337071e4e59
Christian Baidoo <christian.baidoo@15gifts.com>
2026-02-09 19:50:10 +0000

    first attempt find the unique string kata solved

---
+++ findTheUniqueString.ts
@@ -0,0 +1,20 @@
+/* 
+There is an array of strings. All strings contains similar letters except one. Try to find it!
+
+findUniq([ 'Aa', 'aaa', 'aaaaa', 'BbBb', 'Aaaa', 'AaAaAa', 'a' ]) === 'BbBb'
+findUniq([ 'abc', 'acb', 'bac', 'foo', 'bca', 'cab', 'cba' ]) === 'foo'
+Strings may contain spaces. Spaces are not significant, only non-spaces symbols matters. E.g. string that contains only spaces is like empty string.
+
+It’s guaranteed that array contains more than 2 strings.
+
+*/
+
+export function findUniq(arr: string[]): string {
+  const arrays = ["abc", "acb", "bac", "foo", "bca", "cab", "cba"].map((a) => {
+    return new Set([...a.toLowerCase()]);
+  });
+
+  console.log(arrays);
+
+  return arr[0];
+}
