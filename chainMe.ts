18848febde735b4ba6e22735080eee3ba21e8872
Christian Baidoo <christian.baidoo@15gifts.com>
2026-04-20 21:20:24 +0100

    solve chain me kata

---
+++ chainMe.ts
@@ -0,0 +1,11 @@
+/*
+Write a generic function chainer that takes a starting value, and an array of functions to execute on it (array of symbols for Ruby).
+The input for each function is the output of the previous function (except the first function, which takes the starting value as its input). 
+Return the final value after execution is complete.
+*/
+
+function chainFunctions<T>(startValue: T, fns: Array<(arg: T) => T>): T {
+  return fns.reduce((accumulator, currentFunction) => {
+    return currentFunction(accumulator);
+  }, startValue);
+}
